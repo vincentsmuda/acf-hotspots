4115ba066aab3dd9253890c7432f4be6943a7acb
Vincent Smuda <vincent@vincentsmuda.com>
2016-12-10 08:48:21 -0500

    added plugin info

--- acf-hotspots/index.php
+++ acf-hotspots/index.php
@@ -1,7 +1,16 @@
 <?php
 
+	/**
+	 *	Plugin Name: ACF - Hotspots
+ 	 *  Plugin URI: https://github.com/thefreshvince/acf-hotspots/
+ 	 *  Description: A hotspot extension for Advanced Custom Fields
+ 	 *  Author: Vincent Smuda
+ 	 *  Version: 0.1
+ 	 *  Author URI: http://vincentsmuda.com
+	 */
+
   /**
-   *  Setup helper functions
+   *  Include helper functions
    */
   include 'parts/functions/helper-functions.php';
 
